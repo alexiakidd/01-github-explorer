f26b87b2370214a0b15c5a30ab869103156eb57a
Alessandra Lima <alessandra.lima@southsystem.com.br>
2022-02-09 20:31:26 -0300

    feat: components

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import RepositoryItem from './RepositoryItem';
+import '../styles/repositories.scss';
+//https://api.github.com/orgs/Rocketseat/repos
+const repository = {
+	name: 'unform',
+	description: 'Forms in React',
+	link: 'https://github.com/unform/unform',
+};
+
+export function RepositoryList() {
+	const [repositories, setRepositories] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/orgs/Rocketseat/repos')
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}, []);
+
+	return (
+		<section className="repository-list">
+			<h1>Lista de repositórios</h1>
+			<ul>
+				<RepositoryItem repository={repository} />
+				<RepositoryItem repository={repository} />
+				<RepositoryItem repository={repository} />
+			</ul>
+		</section>
+	);
+}
