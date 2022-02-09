5494e0868c37613d576f0a1268cd6fa17b9c7d2c
Alessandra Lima <alessandra.lima@southsystem.com.br>
2022-02-09 20:30:13 -0300

    chore: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const isDevelopment = process.env.NODE_ENV !== 'production'
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 module.exports = {
 	mode: isDevelopment ? 'development' : 'production',
 	devtool: isDevelopment ? 'eval-source-map' : 'source-map',
@@ -15,19 +15,29 @@ module.exports = {
 	},
 	devServer: {
 		static: path.resolve(__dirname, 'public'),
+		hot: true,
 	},
 	plugins: [
+		isDevelopment && new ReactRefreshWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'public', 'index.html'),
 		}),
-	],
+	].filter(Boolean),
 	module: {
 		rules: [
 			{
 				test: /\.jsx$/,
 				exclude: /node_modules/,
-				use: 'babel-loader',
+				use: {
+					loader: 'babel-loader',
+					options: {
+						plugins: [
+							isDevelopment && require.resolve('react-refresh/babel'),
+						].filter(Boolean),
+					},
+				},
 			},
+
 			{
 				test: /\.scss$/,
 				exclude: /node_modules/,
@@ -35,4 +45,4 @@ module.exports = {
 			},
 		],
 	},
-}
+};
